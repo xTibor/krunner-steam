80919064de29068d94ae5fd09206a5e4578206e2
Nagy Tibor <xnagytibor@protonmail.com>
2023-04-26 18:36:56 +0200

    Update blacklist

--- src/blacklist.py
+++ src/blacklist.py
@@ -5,5 +5,7 @@ appid_blacklist = [
     "1391110", # Steam Linux Runtime - Soldier
     "1493710", # Proton Experimental
     "1580130", # Proton 6.3
+    "1628350", # Steam Linux Runtime - Sniper
     "1887720", # Proton 7.0
+    "2348590", # Proton 8.0
 ]
