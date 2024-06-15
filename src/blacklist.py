d00c52cca100172b0d4ae588965fa2f92ad445ee
Nagy Tibor <xnagytibor@protonmail.com>
2024-06-15 18:02:09 +0200

    Update blacklist

--- src/blacklist.py
+++ src/blacklist.py
@@ -2,10 +2,22 @@
 
 appid_blacklist = [
     "228980",  # Steamworks Common Redistributables
+    "858280",  # Proton 3.7
+    "961940",  # Proton 3.16
+    "1054830", # Proton 4.2
+    "1070560", # Steam Linux Runtime - Scout
+    "1113280", # Proton 4.11
+    "1161040", # Proton BattlEye Runtime
+    "1245040", # Proton 5.0
     "1391110", # Steam Linux Runtime - Soldier
+    "1420170", # Proton 5.13
     "1493710", # Proton Experimental
     "1580130", # Proton 6.3
     "1628350", # Steam Linux Runtime - Sniper
+    "1826330", # Proton EasyAntiCheat Runtime
     "1887720", # Proton 7.0
+    "2180100", # Proton Hotfix
+    "2230260", # Proton Next
     "2348590", # Proton 8.0
+    "2805730", # Proton 9.0
 ]
